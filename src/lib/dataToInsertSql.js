ad88954a3222be1ff06aa8a6c730cc0cbb594540
vladimirbuskin <vladimir.buskin@gmail.com>
2018-09-14 16:50:40 -0700

    test checks number added

--- src/lib/dataToInsertSql.js
+++ src/lib/dataToInsertSql.js
@@ -1,12 +1,13 @@
 export default function data(table, data, key) {
 
+  if (!Array.isArray(data)) throw new Error('data is not array, it is "' + typeof(data) + '"');
   if (data.length === 0) return 0;
 
   let r = data[0];
   let fields = Object.keys(r);
-
+  
   let values = data.map(r => '(' + fields.map(f => quote(r[f])).join(',') + ')')
-
+  
   let keyIsEmpty = (key === '' || key == null);
 
   let sql = `insert into [${table}] (${fields.map(f=>'['+f+']')}) ${ keyIsEmpty ? '':`output inserted.[${key}] `}values ${values.join(',')}`;
