955840edddaad756a17731c1a4140d5cf441b5a0
Vladimir Buskin <vladimir.buskin@gmail.com>
2019-05-07 19:12:47 -0700

    better error description that number is not allowed

--- src/lib/mapper.js
+++ src/lib/mapper.js
@@ -1,56 +1,57 @@
 export default function mapper(def, rec, map) {
-	var r = def;
+  var r = def;
 
-	// // validate that all fields are processed
-	// Object.keys(rec).forEach(k => {
-	// 	var v = rec[k]
-	// 	if (k === '' && v) {
-	// 		console.warn('csv key is empty, but value exists');
-	// 	}
-	// 	if (k !== '') {			
-	// 		var cfg = map.find(m => m.value == k)
-	// 		if (cfg) {
-	// 		}
-	// 		else
-	// 		{
-	// 			console.warn('Map is not found for "' + k + '" field.');
-	// 		}
-	// 	}
-	// })
+  // // validate that all fields are processed
+  // Object.keys(rec).forEach(k => {
+  //  var v = rec[k]
+  //  if (k === '' && v) {
+  //    console.warn('csv key is empty, but value exists');
+  //  }
+  //  if (k !== '') {     
+  //    var cfg = map.find(m => m.value == k)
+  //    if (cfg) {
+  //    }
+  //    else
+  //    {
+  //      console.warn('Map is not found for "' + k + '" field.');
+  //    }
+  //  }
+  // })
 
-	// map fields
-	map.forEach(m => {
-		
-		var {
-			name,
-			value
-		} = m
-		
-		if (value == null) throw new Error('"value" is required for field mapping, if not needed specify empty string: ', m)
-		if (value === '') console.warn('"value" is empty, not recommended', m);
+  // map fields
+  map.forEach(m => {
+    
+    var {
+      name,
+      value
+    } = m
+    
+    if (value == null) throw new Error('"value" is required for field mapping, if not needed specify empty string: ', m)
+    if (value === '') console.warn('"value" is empty, not recommended', m);
 
-		let v;
-		if (typeof(value) === 'string') {
-			v = rec[value];
-			if (!rec.hasOwnProperty(value)) {
-				throw new Error(`property "${value}" is not found.`, m)
-			}
-		}
-		else {
-			v = value(rec)
-		}
+    let v;
+    if (typeof value === 'string') {
+      v = rec[value];
+      if (!rec.hasOwnProperty(value)) {
+        throw new Error(`property "${value}" is not found.`, m);
+      }
+    } else if (typeof (value) === 'function') {
+      v = value(rec);
+    } else {
+      throw new Error('mapper value could be string => "ColumnName" or function which is mapper func. if you want const value, put in mapper func 1st param')
+    }
 
-		// it means value is string
-		if (m.length) {
-			v = v + ''
-			if (m.length < v.length)
-				throw new Error(`value "${value}" is longer than ${m.length} characters: `+ JSON.stringify(rec))
-		}
+    // it means value is string
+    if (m.length) {
+      v = v + ''
+      if (m.length < v.length)
+        throw new Error(`value "${value}" is longer than ${m.length} characters: `+ JSON.stringify(rec))
+    }
 
-		// skip empty name
-		if (name !== '')
-			r[name] = v;
-	})
+    // skip empty name
+    if (name !== '')
+      r[name] = v;
+  })
 
-	return r;
+  return r;
 }
--- src/lib/mapper.test.js
+++ src/lib/mapper.test.js
@@ -1,103 +1,122 @@
 import mapper from './mapper'
 
 var d = {
-	"fname": "vladimir", 
-	"lname":'buskin', 
-	"address": "157-166 pushkinskaya" 
+  "fname": "vladimir", 
+  "lname":'buskin', 
+  "address": "157-166 pushkinskaya" 
 }
 
 describe('mapper', function() {
 
 
-	it('works', function() {
+  it('works', function() {
 
-		var r = mapper(
-			{},
-			d,
-			[
-				{"name":"firstName", value:"fname"},
-				{"name":"lastName", value:"lname"},
-				{"name":"workAddress", value:"address"},
-				{"name":"fullName", value: d => d['fname'] + d['lname']},
-			]
-		)
+    var r = mapper(
+      {},
+      d,
+      [
+        {"name":"firstName", value:"fname"},
+        {"name":"lastName", value:"lname"},
+        {"name":"workAddress", value:"address"},
+        {"name":"fullName", value: d => d['fname'] + d['lname']},
+      ]
+    )
 
-		expect(r).deep.equal({
-			firstName:"vladimir",
-			lastName:"buskin",
-			workAddress: "157-166 pushkinskaya",
-			fullName:"vladimirbuskin",
-		})
-	})
+    expect(r).deep.equal({
+      firstName:"vladimir",
+      lastName:"buskin",
+      workAddress: "157-166 pushkinskaya",
+      fullName:"vladimirbuskin",
+    })
+  })
 
-	it('skipEmpty name', function() {
+  it('skipEmpty name', function() {
 
-		var r = mapper(
-			{},
-			d,
-			[
-				{name:"firstName", value:"fname"},
-				{name:"lastName", value:"lname"},
-				{name:"", value:"address"},
-			]
-		)
+    var r = mapper(
+      {},
+      d,
+      [
+        {name:"firstName", value:"fname"},
+        {name:"lastName", value:"lname"},
+        {name:"", value:"address"},
+      ]
+    )
 
-		expect(r).deep.equal({
-			firstName:"vladimir",
-			lastName:"buskin"
-		})
-	})
+    expect(r).deep.equal({
+      firstName:"vladimir",
+      lastName:"buskin"
+    })
+  })
 
-	it('error', function() {
+  it('error', function() {
 
-		var er = null;
-		try {
-			var r = mapper(
-				{},
-				d,
-				[
-					{"name":"firstName", value:"fname"},
-					{"name":"lastName", value:"lname"},
-					{"name":"workAddress", value:"address"},
-					{"name":"fullName", value: d => d['fname'] + d['lname']},
-					{"name":"nonExisting", value:"nonExisting"},
-				]
-			)
-		}
-		catch(e) {
-			er = e;
-		}
-		expect(er).not.equal(null)
-	})
+    var er = null;
+    try {
+      var r = mapper(
+        {},
+        d,
+        [
+          {"name":"firstName", value:"fname"},
+          {"name":"lastName", value:"lname"},
+          {"name":"workAddress", value:"address"},
+          {"name":"fullName", value: d => d['fname'] + d['lname']},
+          {"name":"nonExisting", value:"nonExisting"},
+        ]
+      )
+    }
+    catch(e) {
+      er = e;
+    }
+    expect(er).not.equal(null)
+  })
 
-	it('length works', function() {
-		var r = mapper(
-			{},
-			d,
-			[
-				{"name":"firstName", value:"fname", length: 10},
-				{"name":"lastName", value:"lname"},
-			]
-		)
-	})
+  it('error 2', function() {
+    var er = null;
+    try {
+      var r = mapper(
+        {},
+        d,
+        [
+          // numbers are not allowed, const should go to first param, or as function () => 1
+          {"name":"column1", value:2},
+        ]
+      )
+    }
+    catch(e) {
+      er = e;
+    }
+    expect(er).not.equal(null);
+    expect(er.message.indexOf('mapper value could be string')).equal(0);
+  })
 
-	it('length error', function() {
+  it('length works', function() {
+    var r = mapper(
+      {},
+      d,
+      [
+        {"name":"firstName", value:"fname", length: 10},
+        {"name":"lastName", value:"lname"},
+      ]
+    )
+  })
 
-		var er = null;
-		try {
-			var r = mapper(
-				{},
-				d,
-				[
-					{"name":"firstName", value:"fname", length: 3},
-					{"name":"lastName", value:"lname"},
-				]
-			)
-		}
-		catch(e) {
-			er = e.message;
-		}
-		expect(er).equal('value "fname" is longer than 3 characters: {"fname":"vladimir","lname":"buskin","address":"157-166 pushkinskaya"}')
-	})
+  it('length error', function() {
+
+    var er = null;
+    try {
+      var r = mapper(
+        {},
+        d,
+        [
+          {"name":"firstName", value:"fname", length: 3},
+          {"name":"lastName", value:"lname"},
+        ]
+      )
+    }
+    catch(e) {
+      er = e.message;
+    }
+    expect(er).equal('value "fname" is longer than 3 characters: {"fname":"vladimir","lname":"buskin","address":"157-166 pushkinskaya"}')
+  })
 
 })
